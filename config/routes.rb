bf1d69c3a876313764c9d396505d953a89911b9e
raba <barabara0224@gmail.com>
2019-07-11 03:50:10 +0900

    ログイン

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
+  
   get '/signup' =>'users#new'
   post '/signup' =>'users#create'
+
  root 'game_share#home'
  get '/help' =>'game_share#help'
+ 
+ get '/login' =>'sessions#new'
+ post '/login' =>'sessions#create'
+ delete '/logout' =>'sessions#destroy'
+
  resources :users
 end
